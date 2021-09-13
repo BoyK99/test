18b1771c24a375ea44589761bb14c62dfa52a8f5
BoyK99 <89838779+BoyK99@users.noreply.github.com>
2021-09-13 16:32:29 +0200

    Add files via upload
    index.php geupload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        <title>To-Do School werk</title>
+        <link rel="icon" type="image/png" href="image/favicon.png" sizes="96x96">
+    </head>
+    <body>
+        <?php include_once('navbar.php'); ?>
+        Homepage
+    </body>
+</html>
+<?php include_once('footer.php'); ?>
